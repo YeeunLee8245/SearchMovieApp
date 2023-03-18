a06e06293d434681982ca3568a858892d2346c93
Yeeun Lee <leeyeah8245@gmail.com>
2023-03-19 00:52:52 +0900

    [Add Codes] 라이브러리 세팅

--- app/src/main/java/kr/co/yeeun/lee/demoi/searchmovieapp/ui/activity/MainActivity.kt
+++ app/src/main/java/kr/co/yeeun/lee/demoi/searchmovieapp/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package kr.co.yeeun.lee.demoi.searchmovieapp
+package kr.co.yeeun.lee.demoi.searchmovieapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.yeeun.lee.demoi.searchmovieapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
