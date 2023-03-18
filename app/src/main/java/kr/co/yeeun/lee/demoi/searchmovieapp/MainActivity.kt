261d0656e30218ec9a01b905f03cdba3864a9fc6
Yeeun Lee <leeyeah8245@gmail.com>
2023-03-18 23:34:36 +0900

    init commit

---
+++ app/src/main/java/kr/co/yeeun/lee/demoi/searchmovieapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.yeeun.lee.demoi.searchmovieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
